24f40b0f38876edea38817c7c279282eafc1470a
Khristian Punzalan <khrispunzalan@gmail.com>
2019-05-30 21:00:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
-def prime?(num)
-  (2..(num - 1)).each do |n|
-  return false if num % n == 0
+def prime?(n) 
+  return false if n < 2
+  return true if n == 2
+  (2..Math.sqrt(n).ceil).each do |x|
+   return false if n % x == 0
   end
   true
 end
